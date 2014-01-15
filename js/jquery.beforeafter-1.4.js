80fc1a1e3c9e3aba38b7214e3ae272dd7be487ee
Elina <elina.sundukova@gmail.com>
2014-01-15 16:34:54 +0100

    animations updated

--- js/jquery.beforeafter-1.4.js
+++ js/jquery.beforeafter-1.4.js
@@ -112,7 +112,7 @@
 				});
 			}
 
-			$('#dragwrapper').draggable( { containment:obj,drag:drag,stop:drag });
+			$('#dragwrapper'+randID).draggable( { containment:obj,drag:drag,stop:drag });
 
 			function drag()
 			{
